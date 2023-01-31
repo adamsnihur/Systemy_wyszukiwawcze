f078e8f5ed57b1e393b555f6018f247196a698c6
adamsnihur <102862058+adamsnihur@users.noreply.github.com>
2023-01-31 16:43:21 +0100

    Add files via upload

---
+++ URUCHOMIENIE KODÓW 2.py
@@ -0,0 +1,4 @@
+import subprocess
+
+subprocess.run(['python', '/Users/joachimuetake/SW projekt/AVANS/FINAL SCRAP 6.py'])
+subprocess.run(['python', '/Users/joachimuetake/SW projekt/RTV euro AGD/SCRAPING + FORMATOWANIE 2.py'])
